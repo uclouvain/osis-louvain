7b4b8b4f63275e18329dced55403da86ab9eef84
Christophe Fivet <christophe.fivet@uclouvain.be>
2018-05-18 16:26:44 +0200

    Correction test
    	modifié :         base/tests/forms/test_learning_unit_create_2.py

--- base/tests/forms/test_learning_unit_create_2.py
+++ base/tests/forms/test_learning_unit_create_2.py
@@ -153,14 +153,12 @@ class TestFullFormInit(LearningUnitFullFormContextMixin):
         form = FullForm(self.person, self.learning_unit_year.academic_year,
                         learning_unit_instance=self.learning_unit_year.learning_unit)
         disabled_fields = {key for key, value in form.fields.items() if value.disabled}
-        disabled_fields.add('internship_subtype')
         self.assertEqual(disabled_fields, FULL_READ_ONLY_FIELDS)
 
     def test_disable_fields_full_proposal(self):
         form = FullForm(self.person, self.learning_unit_year.academic_year,
                         learning_unit_instance=self.learning_unit_year.learning_unit, proposal=True)
         disabled_fields = {key for key, value in form.fields.items() if value.disabled}
-        disabled_fields.add('internship_subtype')
         self.assertEqual(disabled_fields, FULL_PROPOSAL_READ_ONLY_FIELDS)
 
     def test_disable_internship_subtype(self):
