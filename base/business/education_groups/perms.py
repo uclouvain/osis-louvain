bc7ad598d13232032dbb43dd2e930c850e4a645d
BenJ <benjamin.daubry@uclouvain.be>
2018-11-28 13:10:14 +0100

    OSIS-2265 : edit perms

--- base/business/education_groups/perms.py
+++ base/business/education_groups/perms.py
@@ -203,8 +203,7 @@ def is_eligible_to_edit_general_information(person, education_group, raise_excep
 
 
 def _is_eligible_to_edit_general_information(person, education_group, raise_exception):
-    return (person.is_central_manager() and education_group.is_common) or \
-           check_link_to_management_entity(education_group, person, raise_exception) and \
+    return check_link_to_management_entity(education_group, person, raise_exception) and \
            (
                    person.is_central_manager() or
                    is_academic_calendar_opened(
