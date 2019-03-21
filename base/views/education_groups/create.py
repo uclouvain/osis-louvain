563f4d9c85bf3a329cc48bafd1945bd9b0ec01bc
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2019-03-21 11:10:28 +0100

    #OSIS-2671 Review

--- base/views/education_groups/create.py
+++ base/views/education_groups/create.py
@@ -146,9 +146,8 @@ def _common_success_redirect(request, form, parent=None):
 
 
 def _get_success_message_for_creation_education_group_year(parent_id, education_group_year):
-    msg_key = _("Education group year <a href='%(link)s'> %(acronym)s (%(academic_year)s) </a> successfully created.")
     link = reverse("education_group_read", args=[parent_id, education_group_year.id])
-    return _(msg_key) % {
+    return _("Education group year <a href='%(link)s'> %(acronym)s (%(academic_year)s) </a> successfully created.") % {
         "link": link,
         "acronym": education_group_year.acronym,
         "academic_year": education_group_year.academic_year,
