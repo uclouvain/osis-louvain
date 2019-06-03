2383544a0e724ba29773036f385cfa42a2a2d541
BenJ <benjamin.daubry@uclouvain.be>
2019-06-03 15:25:51 +0200

    Perms for LU

--- base/tests/business/learning_units/test_perms.py
+++ base/tests/business/learning_units/test_perms.py
@@ -25,20 +25,20 @@
 ##############################################################################
 from unittest import mock
 
-from django.test import TestCase
 from django.core.exceptions import PermissionDenied
+from django.test import TestCase
 
-from base.business.learning_units.perms import is_eligible_to_modify_end_year_by_proposal, \
-    is_eligible_to_modify_by_proposal
 from base.business.learning_units.perms import MSG_NOT_ELIGIBLE_TO_MODIFY_END_YEAR_PROPOSAL_ON_THIS_YEAR
-from base.tests.factories.learning_unit_year import LearningUnitYearFactory
-from base.tests.factories.learning_unit import LearningUnitFactory
-from base.tests.factories.learning_container_year import LearningContainerYearFactory
-from base.tests.factories.person import FacultyManagerFactory
-from base.tests.factories.proposal_learning_unit import ProposalLearningUnitFactory
-from base.tests.factories.academic_year import create_current_academic_year, AcademicYearFactory
-from base.models.enums import learning_unit_year_subtypes
+from base.business.learning_units.perms import is_eligible_to_modify_end_year_by_proposal, \
+    is_eligible_to_modify_by_proposal, MSG_NOT_ELIGIBLE_TO_PUT_IN_PROPOSAL_ON_THIS_YEAR, is_eligible_for_modification
 from base.models.enums import learning_container_year_types
+from base.models.enums import learning_unit_year_subtypes
+from base.tests.factories.academic_year import create_current_academic_year, AcademicYearFactory
+from base.tests.factories.learning_container_year import LearningContainerYearFactory
+from base.tests.factories.learning_unit import LearningUnitFactory
+from base.tests.factories.learning_unit_year import LearningUnitYearFactory
+from base.tests.factories.person import FacultyManagerFactory, CatalogViewerFactory
+from base.tests.factories.proposal_learning_unit import ProposalLearningUnitFactory
 
 
 class TestPerms(TestCase):
@@ -84,8 +84,6 @@ class TestPerms(TestCase):
                                                                    person_faculty_manager,
                                                                    True))
 
-
-
     @mock.patch("base.business.learning_units.perms.is_eligible_to_create_modification_proposal", return_value=True)
     def test_not_is_eligible_to_modify_by_proposal(self, mock_perm):
 
@@ -100,9 +98,11 @@ class TestPerms(TestCase):
         person_faculty_manager = FacultyManagerFactory()
 
         with self.assertRaises(PermissionDenied) as perm_ex:
-            is_eligible_to_modify_by_proposal(learning_unit_yr,
-                                                       person_faculty_manager,
-                                                       True)
+            is_eligible_to_modify_by_proposal(
+                learning_unit_yr,
+                person_faculty_manager,
+                True
+            )
             self.assertEqual('{}'.format(perm_ex.exception), MSG_NOT_ELIGIBLE_TO_PUT_IN_PROPOSAL_ON_THIS_YEAR)
 
     @mock.patch("base.business.learning_units.perms.is_eligible_to_create_modification_proposal", return_value=True)
@@ -116,6 +116,13 @@ class TestPerms(TestCase):
         )
         ProposalLearningUnitFactory(learning_unit_year=learning_unit_yr)
         person_faculty_manager = FacultyManagerFactory()
-        self.assertTrue(is_eligible_to_modify_by_proposal(learning_unit_yr,
-                                                                   person_faculty_manager,
-                                                                   True))
+        self.assertTrue(is_eligible_to_modify_by_proposal(
+            learning_unit_yr,
+            person_faculty_manager,
+            True
+        ))
+
+    def test_is_not_eligible_to_modify_cause_user_is_catalog_viewer(self):
+        cv = CatalogViewerFactory()
+        luy = LearningUnitYearFactory(learning_unit=self.learning_unit, learning_container_year=self.lcy)
+        self.assertFalse(is_eligible_for_modification(luy, cv))
