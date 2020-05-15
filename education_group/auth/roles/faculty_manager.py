a7824fc8e2c610dfece0171ffb965247f4597fa5
Mathieu Zen <zen.mathieu@gmail.com>
2020-05-15 12:09:37 +0200

    OSIS-4703 disable creation in other management entity

--- education_group/auth/roles/faculty_manager.py
+++ education_group/auth/roles/faculty_manager.py
@@ -37,9 +37,11 @@ class FacultyManager(EducationGroupTypeScopeRoleMixin, osis_role_models.EntityRo
                 {"category": Categories.TRAINING.value}
             ),
             'base.add_minitraining':
+                predicates.is_user_attached_to_management_entity &
                 predicates.is_program_edition_period_open &
                 predicates.is_maximum_child_not_reached_for_mini_training_category,
             'base.add_group':
+                predicates.is_user_attached_to_management_entity &
                 predicates.is_not_orphan_group &
                 predicates.is_program_edition_period_open &
                 predicates.is_maximum_child_not_reached_for_group_category,
