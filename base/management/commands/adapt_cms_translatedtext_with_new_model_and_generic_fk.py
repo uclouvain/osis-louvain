204c367a8636c0ee9f63d4912f811a5c4a5663ab
BenJ <benjamin.daubry@uclouvain.be>
2020-06-24 21:30:55 +0200

    OSIS-4802 : starting script to migrate cms_translatedtext

---
+++ base/management/commands/adapt_cms_translatedtext_with_new_model_and_generic_fk.py
@@ -0,0 +1,60 @@
+from django.contrib.contenttypes.models import ContentType
+
+from base.models.education_group_year import EducationGroupYear
+from base.models.learning_unit_year import LearningUnitYear
+from cms.models.translated_text import TranslatedText
+from education_group.models.group_year import GroupYear
+from osis_common.utils.models import get_object_or_none
+
+# Update attributes for generic fk --- Learning Unit Year
+to_update = []
+luy_cms = TranslatedText.objects.filter(
+    entity='learning_unit_year',
+    reference_type__isnull=True
+)
+fk_type = ContentType.objects.get_for_model(LearningUnitYear)
+for cms in luy_cms:
+    luy = LearningUnitYear.objects.get(id=cms.reference)
+    cms.reference_type_id = fk_type.id
+    cms.reference_object = luy
+    to_update.append(cms)
+TranslatedText.objects.bulk_update(to_update, ['reference_type_id', 'reference_object'], batch_size=1000)
+
+# Update attributes for generic fk --- Trainings & Mini trainings
+to_update = []
+offers_cms = TranslatedText.objects.filter(
+    entity='offer_year',
+    reference_type__isnull=True
+)
+fk_type = ContentType.objects.get_for_model(EducationGroupYear)
+for cms in offers_cms:
+    egy = get_object_or_none(
+        EducationGroupYear,
+        id=cms.reference
+    )
+    if egy:
+        cms.reference_type_id = fk_type.id
+        cms.reference_object = egy
+        to_update.append(cms)
+TranslatedText.objects.bulk_update(to_update, ['reference_type_id', 'reference_object'], batch_size=1000)
+
+# Update attributes for generic fk --- Groups
+# to_update = []
+# groups_cms = TranslatedText.objects.filter(
+#     entity='offer_year',
+#     reference_type__isnull=True
+# )
+# fk_type = ContentType.objects.get_for_model(GroupYear)
+# for cms in groups_cms:
+#     group = GroupYear.objects.filter(
+#         # HOW TO GET CORRECT GROUP_YEAR ???
+#     )
+#     # If old data
+#     # group = GroupYear.objects.filter(
+#     #   educationgroupversion__offer__id=cms.reference
+#     # )
+#     cms.reference = group.id
+#     cms.reference_type_id = fk_type.id
+#     cms.reference_object = group
+#     to_update.append(cms)
+# TranslatedText.objects.bulk_update(to_update, ['reference_type_id', 'reference_object'], batch_size=1000)
