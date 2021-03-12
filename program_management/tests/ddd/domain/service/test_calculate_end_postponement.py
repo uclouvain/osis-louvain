805c95955e73779be40a281fa59047e275459783
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2021-03-12 10:27:14 +0100

    OSIS-3752 : En tant que tous, je ne dois pas pouvoir reporter mes modifications effectuées sur une OF dans le passé pour l'onglet Identification et Diplôme

--- program_management/tests/ddd/domain/service/test_calculate_end_postponement.py
+++ program_management/tests/ddd/domain/service/test_calculate_end_postponement.py
@@ -1,11 +1,11 @@
-# ############################################################################
+#############################################################################
 #  OSIS stands for Open Student Information System. It's an application
 #  designed to manage the core business of higher education institutions,
 #  such as universities, faculties, institutes and professional schools.
 #  The core business involves the administration of students, teachers,
 #  courses, programs and so on.
 #
-#  Copyright (C) 2015-2020 Université catholique de Louvain (http://www.uclouvain.be)
+#  Copyright (C) 2015-2021 Université catholique de Louvain (http://www.uclouvain.be)
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -20,15 +20,15 @@
 #  A copy of this license - GNU General Public License - is available
 #  at the root of the source code of this program.  If not,
 #  see http://www.gnu.org/licenses/.
-# ############################################################################
+#############################################################################
 from unittest import mock
 
-from django.test import TestCase
+from django.test import TestCase, override_settings
 
 from base.tests.factories.academic_year import AcademicYearFactory
 from education_group.tests.ddd.factories.group import GroupFactory
 from education_group.tests.ddd.factories.repository.fake import get_fake_training_repository, \
-    get_fake_mini_training_repository, get_fake_group_repository
+    get_fake_mini_training_repository
 from education_group.tests.ddd.factories.training import TrainingFactory
 from education_group.tests.factories.mini_training import MiniTrainingFactory
 from program_management.ddd.domain.program_tree_version import STANDARD
@@ -61,6 +61,7 @@ class TestCalculateEndPostponementYear(MockPatcherMixin, TestCase):
         self.program_tree_version = ProgramTreeVersionFactory(
             tree=self.program_tree,
             entity_id__version_name=STANDARD,
+            entity_id__year=self.current_year,
             program_tree_repository=self.fake_program_tree_repository
         )
         self.fake_program_tree_version_repository = get_fake_program_tree_version_repository(
@@ -82,58 +83,64 @@ class TestCalculateEndPostponementYear(MockPatcherMixin, TestCase):
     def test_when_end_year_is_none(self):
         training = TrainingFactory(entity_identity__year=self.current_year, end_year=None)
         fake_training_repository = get_fake_training_repository([training])
-        result = CalculateEndPostponement.calculate_end_postponement_year_training(
-            identity=training.entity_id,
-            repository=fake_training_repository,
-        )
-        self.assertEqual(result, self.maximum_postponement_year)
+        with override_settings(YEAR_LIMIT_EDG_MODIFICATION=self.current_year - 1):
+            result = CalculateEndPostponement.calculate_end_postponement_year_training(
+                identity=training.entity_id,
+                repository=fake_training_repository,
+            )
+            self.assertEqual(result, self.maximum_postponement_year)
 
     def test_when_end_year_gt_maximum_postponement(self):
         end_year = self.maximum_postponement_year + 5
         training = TrainingFactory(entity_identity__year=self.current_year, end_year=end_year)
         fake_training_repository = get_fake_training_repository([training])
-        result = CalculateEndPostponement.calculate_end_postponement_year_training(
-            identity=training.entity_id,
-            repository=fake_training_repository,
-        )
-        self.assertEqual(result, self.maximum_postponement_year)
+        with override_settings(YEAR_LIMIT_EDG_MODIFICATION=self.current_year - 1):
+            result = CalculateEndPostponement.calculate_end_postponement_year_training(
+                identity=training.entity_id,
+                repository=fake_training_repository,
+            )
+            self.assertEqual(result, self.maximum_postponement_year)
 
     def test_when_end_year_lt_maximum_postponement(self):
         end_year = self.maximum_postponement_year - 3
         training = TrainingFactory(entity_identity__year=self.current_year, end_year=end_year)
         fake_training_repository = get_fake_training_repository([training])
-        result = CalculateEndPostponement.calculate_end_postponement_year_training(
-            identity=training.entity_id,
-            repository=fake_training_repository,
-        )
-        self.assertEqual(result, end_year)
+        with override_settings(YEAR_LIMIT_EDG_MODIFICATION=self.current_year - 1):
+            result = CalculateEndPostponement.calculate_end_postponement_year_training(
+                identity=training.entity_id,
+                repository=fake_training_repository,
+            )
+            self.assertEqual(result, end_year)
 
     def test_when_end_year_equals_maximum_postponement(self):
         end_year = self.maximum_postponement_year
         training = TrainingFactory(entity_identity__year=self.current_year, end_year=end_year)
         fake_training_repository = get_fake_training_repository([training])
-        result = CalculateEndPostponement.calculate_end_postponement_year_training(
-            identity=training.entity_id,
-            repository=fake_training_repository,
-        )
-        self.assertEqual(result, self.maximum_postponement_year)
+        with override_settings(YEAR_LIMIT_EDG_MODIFICATION=self.current_year - 1):
+            result = CalculateEndPostponement.calculate_end_postponement_year_training(
+                identity=training.entity_id,
+                repository=fake_training_repository,
+            )
+            self.assertEqual(result, self.maximum_postponement_year)
 
     def test_calculate_program_tree_version_end_postponement_year(self):
-        result = CalculateEndPostponement.calculate_end_postponement_year_program_tree_version(
-            identity=self.program_tree_version.entity_id,
-            repository=self.fake_program_tree_version_repository,
-        )
-        self.assertEqual(result, self.maximum_postponement_year)
+        with override_settings(YEAR_LIMIT_EDG_MODIFICATION=self.current_year - 1):
+            result = CalculateEndPostponement.calculate_end_postponement_year_program_tree_version(
+                identity=self.program_tree_version.entity_id,
+                repository=self.fake_program_tree_version_repository,
+            )
+            self.assertEqual(result, self.maximum_postponement_year)
 
     def test_calculate_mini_training_end_postponement_year(self):
         end_year = self.maximum_postponement_year - 3
         mini_training = MiniTrainingFactory(entity_identity__year=self.current_year, end_year=end_year)
         fake_mini_training_repository = get_fake_mini_training_repository([mini_training])
-        result = CalculateEndPostponement.calculate_end_postponement_year_mini_training(
-            identity=mini_training.entity_id,
-            repository=fake_mini_training_repository,
-        )
-        self.assertEqual(result, end_year)
+        with override_settings(YEAR_LIMIT_EDG_MODIFICATION=self.current_year - 1):
+            result = CalculateEndPostponement.calculate_end_postponement_year_mini_training(
+                identity=mini_training.entity_id,
+                repository=fake_mini_training_repository,
+            )
+            self.assertEqual(result, end_year)
 
     @mock.patch(
         'program_management.ddd.domain.service.identity_search.ProgramTreeVersionIdentitySearch.get_from_group_identity'
@@ -146,22 +153,35 @@ class TestCalculateEndPostponementYear(MockPatcherMixin, TestCase):
             entity_identity__code=self.program_tree.entity_id.code,
             end_year=end_year
         )
-        result = CalculateEndPostponement.calculate_end_postponement_year_group(
-            identity=group.entity_id,
-            repository=self.fake_program_tree_version_repository,
-        )
-        assertion_msg = "Should take the end year of the version, not of the group " \
-                        "(end year is not pertinent for groups)"
-        self.assertEqual(result, self.maximum_postponement_year, assertion_msg)
+        with override_settings(YEAR_LIMIT_EDG_MODIFICATION=self.current_year - 1):
+            result = CalculateEndPostponement.calculate_end_postponement_year_group(
+                identity=group.entity_id,
+                repository=self.fake_program_tree_version_repository,
+            )
+            assertion_msg = "Should take the end year of the version, not of the group " \
+                            "(end year is not pertinent for groups)"
+            self.assertEqual(result, self.maximum_postponement_year, assertion_msg)
 
     @mock.patch(
         'program_management.ddd.domain.service.identity_search.ProgramTreeVersionIdentitySearch.get_from_group_identity'
     )
     def test_calculate_program_tree_end_postponement_year(self, mock_get_from_group_identity):
         mock_get_from_group_identity.return_value = self.program_tree_version.entity_id
-        end_year = self.maximum_postponement_year - 3
-        result = CalculateEndPostponement.calculate_end_postponement_year_mini_training(
-            identity=self.program_tree_version.entity_id,
-            repository=self.fake_program_tree_version_repository,
-        )
-        self.assertEqual(result, self.maximum_postponement_year)
+        with override_settings(YEAR_LIMIT_EDG_MODIFICATION=self.current_year - 1):
+            result = CalculateEndPostponement.calculate_end_postponement_year_mini_training(
+                identity=self.program_tree_version.entity_id,
+                repository=self.fake_program_tree_version_repository,
+            )
+            self.assertEqual(result, self.maximum_postponement_year)
+
+    def test_when_academic_year_is_lte_year_limit_edg_modification_settings(self):
+        year_limit_edg_modification = 2019
+        training = TrainingFactory(entity_identity__year=year_limit_edg_modification, end_year=None)
+        fake_training_repository = get_fake_training_repository([training])
+
+        with override_settings(YEAR_LIMIT_EDG_MODIFICATION=2019):
+            result = CalculateEndPostponement.calculate_end_postponement_year_training(
+                identity=training.entity_id,
+                repository=fake_training_repository,
+            )
+            self.assertEqual(result, year_limit_edg_modification)
